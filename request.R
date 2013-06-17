d22b55dea26733fe3f3f2136c9b49eb24fa66451
Dan Hammer <dan.s.hammer@gmail.com>
2013-06-17 14:34:31 -0700

    adjusted function names and syntax

--- request.R
+++ request.R
@@ -5,24 +5,24 @@ library(RCurl)
 query <- function(q) {
   ## Accepts the API query for a URL request and returns an R list
   ## from a JSON object
-  base.url <- "http://localhost:8080/api/counts/"
+  base.url <- "http://wip.forma-api.appspot.com/api/counts/"
   req <- paste(base.url, URLencode(q), sep = "")
   print(paste("Request:", req))
   return(fromJSON(getURL(req), unexpected.escape = "skip"))
 }
 
-adminPath <- function(...) {
+admin.path <- function(...) {
   # Given an arbitrary number of levels, will return the appropriate
   # structure for the API call.
 
   ## Example usage:
   ##   > adminPath("idn", 2, 3)
   ##   "idn/2/3"
-  filtered <- as.list(Filter(function(x) {x != ""}, c(...)))
-  return(do.call("paste", c(filtered, sep="/")))
+  filtered <- as.list(Filter(function(x) { !is.null(x) }, c(...)))
+  return(do.call("paste", c(filtered, sep = "/")))
 }
 
-dateRange <- function(begin = "", end = "") {
+date.range <- function(begin = NULL, end = NULL) {
   ## Accepts a date range of the form "%Y-%m-%d" and returns an
   ## appropriately formatted date object.  Note that if a single date
   ## is supplied, then it is assumed that it is the beginning of the
@@ -32,36 +32,36 @@ dateRange <- function(begin = "", end = "") {
   ##   > dateRange(begin = "2005-12-01", end = "2008-04-14")
   ##   "?date=2005-12-01,2008-04-14"
   query.pre <- "?range="
-  if (begin == "" && end == "") {
+  if (is.null(begin) && is.null(end)) {
     return("")
-  } else if (begin != "" && end == "") {
+  } else if (!is.null(begin) && is.null(end)) {
     return(paste(query.pre, begin, sep = ""))
-  } else if (begin != "" && end != "") {
+  } else if (!is.null(begin) && !is.null(end)) {
     return(paste(query.pre, begin, ",", end, sep = ""))
   } else {
     stop("You must supply a valid date range")
   }
 }
 
-aggregateQuery <- function(level = "") {
+agg.admin <- function(level = NULL) {
   ## Accepts a level for aggregation and returns the appropriately
   ## formatted query parameter
-  if (level != "") {
+  if (!is.null(level)) {
     return(paste("?aggregate=", level, sep = ""))
   } else {
     return("")
   }
 }
 
-dataQuery <- function(iso = "", id1 = "", id2 = "", id3 = "",
-                       aggregate.level = "", begin = "", end = "") {
+count.query <- function(iso = NULL, id1 = NULL, id2 = NULL, id3 = NULL,
+                        aggregate.level = NULL, begin = NULL, end = NULL) {
   ## Accepts a specification of the desired data and returns a data
   ## frame of the panel data set
 
   ## Build the structured query and return the results
-  admin <- adminPath(iso, id1, id2, id3)
-  agg   <- aggregateQuery(aggregate.level)
-  date  <- dateRange(begin, end)
+  admin <- admin.path(iso = iso, id1 = id1, id2 = id2, id3 = id3)
+  agg   <- agg.admin(level = aggregate.level)
+  date  <- date.range(begin = begin, end = end)
   q <- paste(admin, agg, date, sep = "")
   res <- query(q)
 
